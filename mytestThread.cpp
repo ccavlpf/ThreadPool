587ebe7fe4c9845c4dd4ccd2a338e6872641c6d1
ccavlpf <406657438@qq.com>
2023-03-28 16:05:27 +0800

    1234

---
+++ mytestThread.cpp
@@ -0,0 +1,26 @@
+#include "ThreadPool.h"
+#include <iostream>
+
+
+int iTotal = 0;
+
+int add(){
+    iTotal ++;
+    std::cout << "id " << std::this_thread::get_id() << "val : " << iTotal << std::endl;
+    return 0;
+}
+
+int main()
+{
+    ThreadPool pool(3);
+    for (size_t i = 0; i < 10000; i++)
+    {
+        pool.enqueue(add);
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+
+    }
+    
+    std::cout << "total :" << iTotal << std::endl;
+
+    return 0;
+}
