9d6659820615a875528b85cf656d05833d5e2f02
ccavlpf <406657438@qq.com>
2023-03-30 14:28:55 +0800

    1234

--- type.cpp
+++ type.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-#include <type_traits>
+#include <future>
 
-template <typename T, typename U>
-auto add(T t, U u) -> decltype(t + u) {
-    return t + u;
+template <class T, class U>
+auto add(T a, U b) -> decltype( a + b ){
+    return a + b;
 }
 
-int main() {
-    int a = 1, b = 2;
-    std::result_of<decltype(&add<int, double>)(int, double)>::type c = add(a, b);
-   // std::result_of<decltype(add<int, double>)&(int, double)>::type c = add(a, b);
-    std::cout << c << std::endl;
+int add1(int a, int b){
+    return a + b;
+}
+
+int main(){
+    std::result_of<decltype((add<int, int>))(int, int)>::type a = add<int, int>(1,2);
+    std::result_of<decltype((add1))(int, int)>::type b = add1(1,2);
     return 0;
 }
-
